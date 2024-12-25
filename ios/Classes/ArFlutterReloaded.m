6d34fee79fb4565b3a3f5fb41cc6bf793476e101
wmt_mobile_jenilg <jenilg@webmobtech.com>
2024-12-25 13:55:59 +0530

    ios changes

--- ios/Classes/ArFlutterReloaded.m
+++ ios/Classes/ArFlutterReloaded.m
@@ -1,6 +1,6 @@
 #import ArFlutterReloaded.h
 #if __has_include(<ar_flutter_reloaded/ar_flutter_reloaded-Swift.h>)
-#import <ar_flutter_plugin/ar_flutter_plugin-Swift.h>
+#import <ar_flutter_reloaded/ar_flutter_plugin-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
