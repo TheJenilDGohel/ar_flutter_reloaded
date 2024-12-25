6f323d44d71f2c7b1de6aacdbe8360152b96af0b
wmt_mobile_jenilg <jenilg@webmobtech.com>
2024-12-25 12:58:16 +0530

    ios changes

--- ios/Classes/ArFlutterReloaded.m
+++ ios/Classes/ArFlutterReloaded.m
@@ -5,7 +5,7 @@
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "ar_flutter_plugin-Swift.h"
+#import "ar_flutter_reloaded-Swift.h"
 #endif
 
 @implementation ArFlutterPlugin
